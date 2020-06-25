2ad02a191fb191b374dc85241f4b834d6f8f4cd2
김주엽 <49600974+Kim-Ju-Yeop@users.noreply.github.com>
2020-06-25 10:09:23 +0900

    [Update] App Version 6
    Server IP를 이전하였습니다.

--- Meals V2/app/src/main/java/org/techtown/project5/network/NetRetrofit.kt
+++ Meals V2/app/src/main/java/org/techtown/project5/network/NetRetrofit.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class NetRetrofit {
     val retrofit : Retrofit = Retrofit.Builder()
-        .baseUrl("http://ec2-13-209-77-143.ap-northeast-2.compute.amazonaws.com:8080/")
+        .baseUrl("http://101.101.211.188:8080/")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
