e9639bd3a6eac3ac0f43cbf2fc0e39bc3a6601bc
Kim-Ju-Yeop <kjy031104@gmail.com>
2020-07-23 14:28:11 +0900

    [ADD] SplashViewModel

--- Meals V2/app/src/main/java/org/techtown/project5/view/splash/SplashActivity.kt
+++ Meals V2/app/src/main/java/org/techtown/project5/view/splash/SplashActivity.kt
@@ -3,17 +3,27 @@ package org.techtown.project5.view.splash
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import org.techtown.project5.R
+import org.techtown.project5.databinding.ActivitySplashBinding
 import org.techtown.project5.view.MainActivity
 import org.techtown.project5.view.base.BaseActivity
+import org.techtown.project5.viewmodel.splash.SplashViewModel
 
 class SplashActivity : BaseActivity() {
+
+    lateinit var binding : ActivitySplashBinding
+    lateinit var viewModel : SplashViewModel
 
     val SPLASH_TIME : Long = 1900
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
+        viewModel = ViewModelProviders.of(this).get(SplashViewModel::class.java)
 
         Handler().postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
---
+++ Meals V2/app/src/main/java/org/techtown/project5/viewmodel/splash/SplashViewModel.kt
@@ -0,0 +1,7 @@
+package org.techtown.project5.viewmodel.splash
+
+import org.techtown.project5.viewmodel.base.BaseViewModel
+
+class SplashViewModel : BaseViewModel() {
+
+}
